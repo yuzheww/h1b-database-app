b190e918f7f7cd22b291fff544122ebbe92daccc
yuzheww <yuzhe.wang@vanderbilt.edu>
2020-11-30 23:39:45 +0800

    final commit

--- insertCase.php
+++ insertCase.php
@@ -2,26 +2,26 @@
 
 if (isset($_POST['submit'])) {
 
-    require_once("conn.php");
+  require_once("conn.php");
 
-    $cnum = $_POST['cnum'];
-    $cstatus = $_POST['cstatus'];
-    $csub = $_POST['csub'];
-    $cdeci = $_POST['cdeci'];
+  $cnum = $_POST['cnum'];
+  $cstatus = $_POST['cstatus'];
+  $csub = $_POST['csub'];
+  $cdeci = $_POST['cdeci'];
 
-    $query = "CALL insertCase(:cnum, :cstatus, :csub, :cdeci)";
+  $query = "CALL insertCase(:cnum, :cstatus, :csub, :cdeci)";
 
-    try {
-        $prepared_stmt = $dbo->prepare($query);
-        $prepared_stmt->bindValue(':cnum', $cnum, PDO::PARAM_STR);
-        $prepared_stmt->bindValue(':cstatus', $cstatus, PDO::PARAM_STR);
-        $prepared_stmt->bindValue(':csub', $csub, PDO::PARAM_STR);
-        $prepared_stmt->bindValue(':cdeci', $cdeci, PDO::PARAM_STR);
-        $prepared_stmt->execute();
-        $result = $prepared_stmt->fetchAll();
-    } catch (PDOException $ex) { // Error in database processing.
-        echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
-    }
+  try {
+    $prepared_stmt = $dbo->prepare($query);
+    $prepared_stmt->bindValue(':cnum', $cnum, PDO::PARAM_STR);
+    $prepared_stmt->bindValue(':cstatus', $cstatus, PDO::PARAM_STR);
+    $prepared_stmt->bindValue(':csub', $csub, PDO::PARAM_STR);
+    $prepared_stmt->bindValue(':cdeci', $cdeci, PDO::PARAM_STR);
+    $prepared_stmt->execute();
+    $result = $prepared_stmt->fetchAll();
+  } catch (PDOException $ex) { // Error in database processing.
+    echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
+  }
 }
 
 ?>
@@ -29,73 +29,73 @@ if (isset($_POST['submit'])) {
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
-    <div id="navbar">
-        <ul>
-        <li><a href="index.html"> Home </a></li>
-			<li><a href="record.php"> View Record </a></li>
-			<li><a href="updateCase.php"> Update Application </a></li>
-			<li><a href="insertCase.php"> Insert Application </a></li>
-			<li><a href="deleteCase.php"> Delete Application </a></li>
-        </ul>
-    </div>
+  <div id="navbar">
+    <ul>
+      <li><a href="index.html"> Home </a></li>
+      <li><a href="record.php"> View Record </a></li>
+      <li><a href="updateCase.php"> Update Application </a></li>
+      <li><a href="insertCase.php"> Insert Application </a></li>
+      <li><a href="deleteCase.php"> Delete Application </a></li>
+    </ul>
+  </div>
 
-    <h1> Insert H1B Application </h1>
+  <h1> Insert H1B Application </h1>
 
-    <form method="post">
-        <table>
+  <form method="post">
+    <table>
+      <tr>
+        <td><span for="cnum">Case Number(*):</span>
+          <input type="text" name="cnum" id="cnum" required></td>
+        <td><span for="cstatus">Case Status:</span><select id="cstatus" name="cstatus">
+            <option value="WITHDRAWN">WITHDRAWN</option>
+            <option value="CERTIFIED">CERTIFIED</option>
+            <option value="DENIED">DENIED</option>
+            <option value="CERTIFIED-WITHDRAWN">CERTIFIED-WITHDRAWN</option>
+            <option value="INPROGRESS">INPROGRESS</option>
+          </select></td>
+      </tr>
+      <tr>
+        <td><span for="csub">Submission Date:</span>
+          <input type="date" name="csub" id="csub"></td>
+        <td><span for="cdeci">Decision Date:</span>
+          <input type="date" name="cdeci" id="cdeci"></td>
+      </tr>
+    </table>
+    <input type="submit" name="submit" value="Insert" class="btn">
+  </form>
+
+
+  <?php
+  if (isset($_POST['submit'])) {
+    if ($result && $prepared_stmt->rowCount() > 0) { ?>
+
+      <h2>Results</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>insert status</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <?php foreach ($result as $row) { ?>
+
             <tr>
-                <td><span for="cnum">Case Number(*):</span>
-                    <input type="text" name="cnum" id="cnum" required></td>
-                <td><span for="cstatus">Case Status:</span><select id="cstatus" name="cstatus">
-                        <option value="WITHDRAWN">WITHDRAWN</option>
-                        <option value="CERTIFIED">CERTIFIED</option>
-                        <option value="DENIED">DENIED</option>
-                        <option value="CERTIFIED-WITHDRAWN">CERTIFIED-WITHDRAWN</option>
-                        <option value="INPROGRESS">INPROGRESS</option>
-                    </select></td>
+              <td><?php echo $row["msg"]; ?></td>
             </tr>
-            <tr>
-                <td><span for="csub">Submission Date:</span>
-                    <input type="date" name="csub" id="csub"></td>
-                <td><span for="cdeci">Decision Date:</span>
-                    <input type="date" name="cdeci" id="cdeci"></td>
-            </tr>
-        </table>
-        <input type="submit" name="submit" value="Insert" class="btn">
-    </form>
+          <?php } ?>
+        </tbody>
+      </table>
 
-
-    <?php
-      if (isset($_POST['submit'])) {
-        if ($result && $prepared_stmt->rowCount() > 0) { ?>
-    
-              <h2>Results</h2>
-
-              <table>
-                <thead>
-                  <tr>
-                    <th>insert status</th>
-                  </tr>
-                </thead>
-                <tbody>
-            
-                  <?php foreach ($result as $row) { ?>
-                
-                    <tr>
-                      <td><?php echo $row["msg"]; ?></td>
-                    </tr>
-                  <?php } ?>
-                </tbody>
-            </table>
-  
-        <?php } else { ?>
-          Sorry No results found for <?php echo $_POST['cnum']; ?>.
-        <?php }
-    } ?>
+    <?php } else { ?>
+      Sorry No results found for <?php echo $_POST['cnum']; ?>.
+  <?php }
+  } ?>
 
 </body>
 
--- record.php
+++ record.php
@@ -16,93 +16,270 @@ if (isset($_POST['submit'])) {
   } catch (PDOException $ex) { // Error in database processing.
     echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
   }
-}else if (isset($_POST['submit2'])) {
-   require_once("conn.php");
+}
+?>
+<?php
+if (isset($_POST['submit2'])) {
+  require_once("conn.php");
+  $low = $_POST['low'];
+  $high = $_POST['high'];
 
-   $low = $_POST['low'];
-   $high = $_POST['high'];
+  $query = "CALL queryInterval(:low, :high)";
 
-   $query = "CALL queryInterval(:low, :high)";
+  try {
+    $prepared_stmt = $dbo->prepare($query);
+    $prepared_stmt->bindValue(':low', $low, PDO::PARAM_STR);
+    $prepared_stmt->bindValue(':high', $high, PDO::PARAM_STR);
+    $prepared_stmt->execute();
+    $result = $prepared_stmt->fetchAll();
+    echo "<script>alert('Searching!')</script>";
+  } catch (PDOException $ex) { // Error in database processing.
+    echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
+  }
+}
+?>
+<?php
+if (isset($_POST['submit3'])) {
+  require_once("conn.php");
+  $stat = $_POST['stat'];
 
-   try {
-     $prepared_stmt = $dbo->prepare($query);
-     $prepared_stmt->bindValue(':low', $low, PDO::PARAM_STR);
-     $prepared_stmt->bindValue(':high', $high, PDO::PARAM_STR);
-     $prepared_stmt->execute();
-     echo "<script>alert('update is successful!')</script>";
-   } catch (PDOException $ex) { // Error in database processing.
-     echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
-     echo "<script>alert('The update is unsuccesful')</script>";
-   }
- }
+  $query = "CALL queryPercentage(:stat)";
 
+  try {
+    $prepared_stmt = $dbo->prepare($query);
+    $prepared_stmt->bindValue(':stat', $stat, PDO::PARAM_STR);
+    $prepared_stmt->execute();
+    $result = $prepared_stmt->fetchAll();
+  } catch (PDOException $ex) { // Error in database processing.
+    echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
+  }
+}
+?>
+<?php
+if (isset($_POST['submit4'])) {
+  require_once("conn.php");
+  $emp = $_POST['emp'];
 
+  $query = "CALL queryEmployer(:emp)";
+
+  try {
+    $prepared_stmt = $dbo->prepare($query);
+    $prepared_stmt->bindValue(':emp', $emp, PDO::PARAM_STR);
+    $prepared_stmt->execute();
+    $result = $prepared_stmt->fetchAll();
+  } catch (PDOException $ex) { // Error in database processing.
+    echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
+  }
+}
 ?>
 
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+
 
 <body>
   <div id="navbar">
     <ul>
-    <li><a href="index.html"> Home </a></li>
-			<li><a href="record.php"> View Record </a></li>
-			<li><a href="updateCase.php"> Update Application </a></li>
-			<li><a href="insertCase.php"> Insert Application </a></li>
-			<li><a href="deleteCase.php"> Delete Application </a></li>
+      <li><a href="index.html"> Home </a></li>
+      <li><a href="record.php"> View Record </a></li>
+      <li><a href="updateCase.php"> Update Application </a></li>
+      <li><a href="insertCase.php"> Insert Application </a></li>
+      <li><a href="deleteCase.php"> Delete Application </a></li>
     </ul>
   </div>
 
   <h1> Query a Case by case number</h1>
   <form method="post">
 
-    <span for="cnum">Case Number(*):</span>
+    <span for="cnum" ,div>Case Number(*):</span>
     <input type="text" name="cnum">
-          <input type="submit" name="submit" value="Submit">
+    <input type="submit" name="submit" value="Submit" class="btn">
   </form>
 
+  <?php
+  if (isset($_POST['submit'])) {
+    if ($result && $prepared_stmt->rowCount() > 0) { ?>
+      <h2,div>Main Results</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Case Number</th>
+              <th>Case Status</th>
+              <th>Date Submitted</th>
+              <th>Decision Date</th>
+              <th>visa_class</th>
+              <th>Employment Start Date</th>
+              <th>Employment End Date</th>
+              <th>Employer Name</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <?php foreach ($result as $row) { ?>
+
+              <tr>
+                <td>
+                  <div><?php echo $row["case_number"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["case_status"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["case_submitted"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["decision_date"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["visa_class"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["employment_start_date"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["employment_end_date"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["employer_name"]; ?>
+                </td>
+
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      <?php } else { ?>
+        Sorry No results found for <?php echo $_POST['cnum']; ?>.
+    <?php }
+  } ?>
+    <form method="post">
+
+      <span for="low">Search total wage from </span>
+      <input type="double" name="low">
+      <span for="high"> to </span>
+      <input type="double" name="high">
+      <input type="submit" name="submit2" value="Submit" class="btn">
+    </form>
     <?php
-if (isset($_POST['submit'])) {
-        if ($result && $prepared_stmt->rowCount() > 0) { ?>
-              <h2>Main Results</h2>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Case Number</th>
-                    <th>Case Status</th>
-                    <th>Date Submitted</th>
-                    <th>Decision Date</th>
-                    <th>visa_class</th>
-                    <th>Employment Start Date</th>
-                    <th>Employment End Date</th>
-                    <th>Employer Name</th>
+    if (isset($_POST['submit2'])) {
+      if ($result && $prepared_stmt->rowCount() > 0) { ?>
+        <h3>Here</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Case Number</th>
+              <th>Case Status</th>
+              <th>Job Title</th>
+              <th>Total Wage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($result as $row) { ?>
+              <tr>
+                <td>
+                  <div><?php echo $row["case_number"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["case_status"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["job_title"]; ?>
+                </td>
+                <td>
+                  <div><?php echo $row["total_wage"]; ?>
+                </td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      <?php } else { ?>
+        Sorry No results found for the given range<?php echo $_POST['']; ?>.
+    <?php }
+    } ?>
+    <form method="post">
 
-                  </tr>
-                </thead>
-                <tbody>
+      <span for="stat">Status of cases </span>
+      <select id="stat" name="stat" size='1'>
+        <option value="CERTIFIED-WITHDRAWN">CERTIFIED-WITHDRAWN</option>
+        <option value="CERTIFIED">CERTIFIED</option>
+        <option value="DENIED">DENIED</option>
+        <option value="WITHDRAWN">WITHDRAWN</option>
+      </select>
+      <input type="submit" name="submit3" value="Submit" class="btn">
+    </form>
+    <?php
+    if (isset($_POST['submit3'])) {
+      if ($result && $prepared_stmt->rowCount() > 0) { ?>
+        <h3>Result</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Total case number</th>
 
-                  <?php foreach ($result as $row) { ?>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($result as $row) { ?>
+              <tr>
+                <td>
+                  <div><?php echo $row["tot"]; ?>
+                </td>
 
-                    <tr>
-                      <td><div><?php echo $row["case_number"];?></td>
-                      <td><div><?php echo $row["case_status"]; ?></td>
-                      <td><div><?php echo $row["case_submitted"]; ?></td>
-                      <td><div><?php echo $row["decision_date"]; ?></td>
-                      <td><div><?php echo $row["visa_class"]; ?></td>
-                      <td><div><?php echo $row["employment_start_date"]; ?></td>
-                      <td><div><?php echo $row["employment_end_date"]; ?></td>
-                      <td><div><?php echo $row["employer_name"]; ?></td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      <?php } else { ?>
+        Sorry No results found for <?php echo $_POST['stat']; ?>.
+    <?php }
+    } ?>
 
-                    </tr>
-                  <?php } ?>
-                </tbody>
-            </table>
-        <?php } else { ?>
-                  Sorry No results found for <?php echo $_POST['cnum']; ?>.
-                <?php }
+    <form method="post">
+
+      <span for="emp">Search Employer by </span>
+      <input type="text" name="emp">
+      <input type="submit" name="submit4" value="Submit" class="btn">
+    </form>
+    <?php
+    if (isset($_POST['submit4'])) {
+      if ($result && $prepared_stmt->rowCount() > 0) { ?>
+        <h3>Here</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Employer Name</th>
+              <th>City</th>
+              <th>State Abbreviation</th>
+              <th>State Fullname</th>
+              <th>State and City</th>
+              <th>Postal Code</th>
+              <th>Country</th>
+              <th>Phone Number</th>
+              <th>NAIC Code</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($result as $row) { ?>
+              <tr>
+                <td><?php echo $row["employer_name"]; ?></td>
+                <td><?php echo $row["employer_city"]; ?></td>
+                <td><?php echo $row["emp_state_abb"]; ?></td>
+                <td><?php echo $row["emp_state_full"]; ?></td>
+                <td><?php echo $row["emp_state_and_city"]; ?></td>
+                <td><?php echo $row["employer_postal_code"]; ?></td>
+                <td><?php echo $row["employer_country"]; ?></td>
+                <td><?php echo $row["employer_phone"]; ?></td>
+                <td><?php echo $row["naic_code"]; ?></td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      <?php } else { ?>
+        Sorry No results found for <?php echo $_POST['emp']; ?>.
+    <?php }
     } ?>
 </body>
 
